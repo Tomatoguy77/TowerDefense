e82bbb484dbc719962d0485f3a6356ecd688b1a0
Sebastiaan Kappert <19070@ma-web.nl>
2015-11-10 14:55:14 +0100

    hero movement on tag based ground added

---
+++ Assets/scripts/EnemyMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class EnemyMovement : MonoBehaviour {
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private Rigidbody2D rig;
+	
+	// Update is called once per frame
+	void Update () {
+        move();
+	}
+
+    private void move()
+    {
+        rig.AddForce(Vector2.right);
+    }
+}
