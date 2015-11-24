5f6df0de382e155dfbc5cb77bc3188cec6183ddf
Sebastiaan Kappert <19070@ma-web.nl>
2015-11-24 09:56:37 +0100

    working pathfinding ready for merge

---
+++ Assets/scripts/HeroMove.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroMove : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _moveSpeed;
+    private Vector3 _moveTarget;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        _moveTarget = transform.position;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckFloor();
+        MoveTo();
+    }
+
+    void CheckFloor() {
+        Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
+        if (hit)
+        {
+            // check what tag the place of the mouse has
+
+            switch (hit.transform.tag)
+            {
+                case "ground":
+                    if (Input.GetMouseButton(0))
+                    {
+                        _moveTarget = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+            Debug.Log(hit.transform.tag);
+        }
+
+    }
+
+    void MoveTo() {
+
+        if (transform.position != _moveTarget)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, _moveTarget, Time.deltaTime * _moveSpeed);
+
+        }
+
+    }
+
+    
+}
---
+++ Assets/scripts/PlayerMove.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+// this using 
+using Pathfinding;
+
+public class PlayerMove : MonoBehaviour
+    {
+     // change this vector to the mouseclick
+     [SerializeField]
+     private Vector2 targetPosition;
+     private Seeker _seeker;
+     private CharacterController _cont;
+
+     private Path path;
+     [SerializeField]
+      private float _Mspeed = 5;
+      //The max distance from the AI to a waypoint for it to continue to the next waypoint
+     private float _nextWayDist = 3;
+      //The waypoint we are currently moving towards
+    private int _currPoint = 0;
+  
+    
+         void Start()
+        {
+            _seeker = GetComponent<Seeker>();
+            _cont = GetComponent<CharacterController>();
+        //Start a new path to the targetPosition, return the result to the OnPathComplete function
+        _seeker.StartPath(transform.position, targetPosition, OnPathComplete);
+
+
+    }
+
+
+
+    void OnPathComplete(Path p)
+        {
+            Debug.Log("Yay, we got a path back, for the Hero. Did it have an error? " + p.error);
+            if (!p.error)
+            {
+                path = p;
+                //Reset the waypoint counter
+                _currPoint = 0;
+            }
+        }
+    void Clicked()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Debug.Log(Input.mousePosition);
+            targetPosition = Input.mousePosition;
+            _seeker.StartPath(transform.position, targetPosition, OnPathComplete);
+
+
+        }
+
+
+
+    }
+    void Update()
+        {
+             Clicked();
+
+            if (path == null)
+                {   
+                    //We have no path to move after yet
+                    return;
+                }
+                if (_currPoint >= path.vectorPath.Count)
+                {
+                    Debug.Log("End Of Path Reached");
+                    return;
+                }
+                //Direction to the next waypoint
+                Vector2 dir = (path.vectorPath[_currPoint] - transform.position).normalized;
+                dir *= _Mspeed * Time.deltaTime;
+                _cont.Move(dir);
+                //Check if we are close enough to the next waypoint
+                //If we are, proceed to follow the next waypoint
+                if (Vector2.Distance(transform.position, path.vectorPath[_currPoint]) < _nextWayDist)
+                {
+                    _currPoint++;
+                    return;
+            }
+        }
+    }
