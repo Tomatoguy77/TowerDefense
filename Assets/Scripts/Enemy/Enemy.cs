09972199aad4f3ff96efcd2258f8654e429f966c
DasArmin <armin.hunter@hotmail.com>
2015-11-26 21:51:45 +0100

    Added Animations and other art.

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,24 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy : MonoBehaviour
+public class Enemy : TempleCollision
 {
     [SerializeField] private GameObject _healthBar;
-    public int health = 100;
+    [SerializeField] private GameObject _victory;
+    public float health = 100;
+    private bool _attacking = false;
+    private Animator _animator;
+
+    public override void TempleCollide(BaseTemple _BT)
+    {
+        base.TempleCollide(_BT);
+        print("Hit");
+        _animator.SetBool("isAttacking", true);
+        InvokeRepeating("AttackTemple", 0.9f, 1);
+    }
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
 	
 	void Update ()
     {
-        transform.Translate(Vector2.right * Time.deltaTime);
+        transform.Translate(Vector2.left * Time.deltaTime);
 
         if (health == 50)
         {
-            _healthBar.transform.localScale = new Vector3(0.8f, 3.2f, 1.6f);
+            _healthBar.transform.localScale = new Vector3(0.9f, 3, 0);
         }
 
         if (health <= 0)
         {
-            print("Enemy Died!");
-            Destroy(this.gameObject);
+            _healthBar.SetActive(false);
+            _animator.SetBool("isDead", true);
+            StartCoroutine("DestroyObject");
         }
 	}
+
+    private void AttackTemple()
+    {
+        print("Attack!");
+        GameObject.Find("Temple").GetComponent<BaseTemple>().health -= 20f;
+    }
+
+    IEnumerator DestroyObject()
+    {
+        //Moet not speed op 0 zetten.
+        yield return new WaitForSeconds(0.9f);
+        GameObject.Find("_ResourceCountScript").GetComponent<ResourceCount>().resourceCounter += 50f;
+        Destroy(this.gameObject);
+    }
 }
--- Assets/Scripts/Tower/Tower.cs
+++ Assets/Scripts/Tower/Tower.cs
@@ -5,14 +5,19 @@ public class Tower : MonoBehaviour
 {
     [SerializeField] private LayerMask _typeEnemyLayer;
     [SerializeField] private GameObject _bullet;
+    [SerializeField] private GameObject _zeusHand;
+    private Animator _animator;
     private GameObject _closestEnemy;
     private Vector2 _towerPos;
+    private Vector2 _bulletPos;
     private float _shootCooldown = 1.5f;
     public float towerRange = 2f;
 
     void Start()
     {
         _towerPos = transform.position;
+        _bulletPos = _zeusHand.transform.position;
+        _animator = GetComponent<Animator>();
         InvokeRepeating("FindGroundEnemy", 0, _shootCooldown);
     }
 
@@ -36,14 +41,20 @@ public class Tower : MonoBehaviour
         if (_closestEnemy != null)
         {
             print("Closest found!");
+            _animator.SetBool("isAttacking", true);
             ShootBullet();
+        }
+
+        else
+        {
+            _animator.SetBool("isAttacking", false);
         }
     }
     
     //Instantiate a bullet prefab.
     void ShootBullet()
     {
-        GameObject bulletObj = Instantiate(_bullet, transform.position, transform.rotation) as GameObject;
+        GameObject bulletObj = Instantiate(_bullet, _bulletPos, transform.rotation) as GameObject;
         bulletObj.GetComponent<TowerBullet>().SetTarget(_closestEnemy);
     }
 
