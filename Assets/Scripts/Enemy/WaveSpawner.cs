abe8963992de85c3dad50817f57e7300537bd3c0
DasArmin <armin.hunter@hotmail.com>
2015-11-26 21:53:32 +0100

    Added animations and other art.

---
+++ Assets/Scripts/Enemy/WaveSpawner.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WaveSpawner : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _medusa;
+    [SerializeField] private GameObject _harpy;
+    private int _waveLevel = 0;
+    private bool _waveActive = false;
+    private List<GameObject> _enemies;
+
+    void Start()
+    {
+        _enemies = new List<GameObject>();
+
+        for(int i = 0; i < 3; i++)
+        {
+            _enemies.Add(_medusa);
+        }
+    }
+
+    void Update()
+    {   
+        if (_waveActive)
+        {
+            switch(_waveLevel)
+            {
+                case 1:
+                    print("Wave 1 START");
+                    StartCoroutine("SpawnWave");
+                    _waveActive = false;
+                    break;
+
+                case 2:
+                    print("Wave 2 START");
+                    StartCoroutine("SpawnWave");
+                    _waveActive = false;
+                    break;
+                    
+                case 3:
+                    print("Wave 3 START");
+                    StartCoroutine("SpawnWave");
+                    _waveActive = false;
+                    break;
+            }
+        }
+    }
+
+    IEnumerator SpawnWave()
+    {
+        switch (_waveLevel)
+        {
+            case 1:
+                _enemies.Add(_harpy);
+                break;
+
+            case 2:
+                _enemies.Add(_harpy);
+                _enemies.Add(_harpy);
+                break;
+        }
+
+        foreach (var spawnObject in _enemies)
+        {
+            GameObject instanceObject = Instantiate(spawnObject, transform.position, transform.rotation) as GameObject;
+            yield return new WaitForSeconds(1.5f);
+        }
+    }
+
+    public void StartWave()
+    {
+        _waveActive = true;
+        GameObject.Find("_WaveCountScript").GetComponent<WaveCount>().waveCounter += 1;
+        _waveLevel += 1;
+    }
+
+    public void StopWave()
+    {
+        _waveActive = false;
+    }
+}
---
+++ Assets/Scripts/Shop/OpenShop.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenShop : MonoBehaviour
+{
+    [SerializeField] private GameObject _turretSelecter;
+
+    private void OnMouseDown()
+    {
+        _turretSelecter.SetActive(true);
+    }
+    
+}
